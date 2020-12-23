4aa32a33a4d816083468e6842d5c885605b472e2
ajitk7 <ajit.kulkarni167@gmail.com>
2020-12-23 18:10:42 +0530

    assignment updated

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,3 +1,24 @@
-const arrayCubeRootToJson = arr => {};
-
+const arrayCubeRootToJson = (arr) => {
+    if(arr.length==0){
+        throw '';
+    }
+    if(arr.includes(true) || arr.includes(false) || arr.includes(null)||arr.includes(undefined)){
+        throw ''; 
+    }
+    let cubeRootData = new Map();
+    arr.forEach(num => {
+        const key=""+num;
+        if(isNaN(num)){
+            throw ''; 
+        }
+        else {
+                cubeRootData.set(key,Math.cbrt(num));
+        }
+    });
+let obj = Object.create(null);
+  for (let [k,v] of cubeRootData) {
+    obj[k] = v;
+  }
+    return obj;
+};
 export { arrayCubeRootToJson };
--- array-utils/arrayUtils.js
+++ array-utils/arrayUtils.js
@@ -1,3 +1,36 @@
+const forEach = function(arr,callback){
+
+    for(let i=0;i<arr.length;i++){
+      callback(arr[i],i,arr);
+    }
+}
+
+const map = function(callback) {
+  let newArray = []
+  for (let i = 0; i < this.length; i++) {
+    let newMappedItem = callback(this[i], i, this)
+    newArray.push(newMappedItem)
+  }
+  return newArray
+}
+
+const reduce =  function(callback, initialValue){
+  for (let i = 0; i < this.length; i++) {
+    initialValue = callback(initialValue, this[i], i, this)
+  }
+  return initialValue
+}
+
+const filter = function(callback){
+  let filtered=[]
+  for(let i=0;i< this.length;i++)
+  {
+     if(callback(this[i]))
+       filtered.push(this[i]) 
+  }
+
+  return filtered
+}
 
 export {
   forEach,
